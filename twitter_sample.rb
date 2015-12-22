5ef0cd4d5f0c3d808a7534c0de4d693c36e71627
Yusuke Mukoyama <yusuke.mukoyama@gmail.com>
2015-12-22 17:46:47 +0900

    Update twitter_sample.rb

--- twitter_sample.rb
+++ twitter_sample.rb
@@ -1,10 +1,10 @@
 require 'json'
 require 'oauth'
 
-consumer_key = '1pOTpFomIMqEGmPTTUlzUkPvI'
-consumer_secret = 'pOSx203U0366Rzl4RmMisUJLxaga39pfBElJ8umjDod1yrSoog'
-access_token =  '111519936-LQofP6WNMjuuneRTITl71M4uW2eApmVvcA1MQych'
-access_token_secret = '65hzML3emomyb1gcfaBY7fuZnBFf32nLNiH4CIDbObb3q'
+consumer_key = '#'
+consumer_secret = '#'
+access_token =  '#'
+access_token_secret = '#'
 
 consumer = OAuth::Consumer.new(
   consumer_key, 
